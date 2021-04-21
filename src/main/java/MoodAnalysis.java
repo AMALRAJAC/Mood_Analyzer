59c99be218a4b730cbe2b84a32a3dabd028dbb76
unknown <acamalraj@gmail.com>
2021-04-21 21:19:11 +0530

    ADD:mood analysis exception

--- src/main/java/MoodAnalysis.java
+++ src/main/java/MoodAnalysis.java
@@ -10,20 +10,15 @@ public class MoodAnalysis {
     private String analyzeMood() throws MoodAnalysisException {
 
         try {
-            if(message==null){
-                throw new MoodAnalysisException("Error occurred ,error type = "+MoodAnalysisException.errorType.NULL_VALUE);
-            }
-            else if(message.length()==0){
-                throw new MoodAnalysisException("Error occurred ,error type = "+MoodAnalysisException.errorType.EMPTY_VALUE);
-            }
-            else if (message.contains("sad")) {
+
+            if (message.contains("sad")) {
                 return "SAD";
 
             } else if (message.contains("happy")) {
                 return "HAPPY";
             }
             else{
-                throw new MoodAnalysisException("given mood is invalid");
+                throw new MoodAnalysisException("invalid entry");
             }
         } catch ( Exception ex) {
           System.out.println("Exception occurred: "+ex);
--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,10 +1,6 @@
 class MoodAnalysisException extends Exception {
 
     private static final String message =null ;
-    public enum errorType {
-        NULL_VALUE,
-        EMPTY_VALUE;
-    }
     public MoodAnalysisException(String message) {
 
         super(message);
